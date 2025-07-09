e1537a2d46eef2c3a4e9c1375af1513124447222
SudhanshuP11 <sudhanshupathak610@gmail.com>
2025-07-09 10:52:48 +0530

    update

---
+++ src/Leetcode/Ques1281.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+import java.util.*;
+//1281. Subtract the Product and Sum of Digits of an Integer
+public class Ques1281 {
+
+    public static int subtractProductAndSum(int n) {
+        int a=1,b=0;
+        while(n>0){
+            int temp = n%10;
+            a=a*temp;
+            b=b+temp;
+            n=n/10;
+        }
+        int c = a-b;
+        return c;
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = subtractProductAndSum(n);
+        System.out.println(result);
+
+    }
+
+}
