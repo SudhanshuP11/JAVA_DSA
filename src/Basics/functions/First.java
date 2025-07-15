3bf041f93570c12f3b06fd044b57defe813be171
SudhanshuP11 <sudhanshupathak610@gmail.com>
2025-07-15 09:55:07 +0530

    update

---
+++ src/Basics/functions/First.java
@@ -0,0 +1,12 @@
+package Basics.functions;
+
+public class First {
+    public void print(){
+        System.out.println("Hello ji: ");
+    }
+
+    public static void main(String[] args) {
+        First obj = new First();
+        obj.print();
+    }
+}
---
+++ src/Leetcode/Basic/Ques1480.java
@@ -0,0 +1,11 @@
+package Leetcode.Basic;
+
+public class Ques1480 {
+    public int[] runningSum(int[] nums) {
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
