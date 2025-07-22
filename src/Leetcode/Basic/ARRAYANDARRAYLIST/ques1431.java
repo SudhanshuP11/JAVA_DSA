9a988d690a6cd5efc5fb30a2a9492806c354a4ab
SudhanshuP11 <sudhanshupathak610@gmail.com>
2025-07-22 10:57:45 +0530

    update

---
+++ src/Leetcode/Basic/ARRAYANDARRAYLIST/ques1431.java
@@ -0,0 +1,25 @@
+package Leetcode.Basic.ARRAYANDARRAYLIST;
+
+public class ques1431 {
+    class Solution {
+        public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+            List<Boolean> result = new ArrayList<>();
+            int g=candies[0];
+            for(int i=1;i<candies.length;i++){
+                if(g<candies[i]){
+                    g=candies[i];
+                }
+            }
+            for(int j=0;j<candies.length;j++){
+                if((candies[j]+extraCandies)>=g){
+                    result.add(true);
+                }
+                else
+                {
+                    result.add(false);
+                }
+            }
+            return result;
+        }
+    }
+}
